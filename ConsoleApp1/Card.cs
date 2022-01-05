650ee0ae633895e55d73d493bf6a86c461511d69
Yanis <yanis@netkoders.com>
2022-01-05 09:58:00 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Card.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Card
+    {
+        public string Card_title { get; set; }
+        public string Card_description { get; set; }
+    }
+}
