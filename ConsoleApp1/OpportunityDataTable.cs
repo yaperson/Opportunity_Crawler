99ce9f5a570c65bb167fbb9b3e0e1122b1ff28d1
yanis <yanis.legrand.1@gmail.com>
2022-01-24 14:50:36 +0100

    création de OpportunityDataTable

---
+++ ConsoleApp1/OpportunityDataTable.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+
+	public class OpportunityDataTable
+	{
+		public DataTable createOpportunityDataTable()
+		{
+			var datatable = new DataTable();
+
+			datatable.Columns.Add(new DataColumn()
+			{
+				ColumnName = "opportunity_id",
+				DataType = typeof(Guid),
+				AllowDBNull = false
+			});
+
+			datatable.Columns.Add(new DataColumn()
+			{
+				ColumnName = "opportunity_title",
+				DataType = typeof(string),
+				AllowDBNull = false
+			});
+
+			datatable.Columns.Add(new DataColumn()
+			{
+				ColumnName = "opportunity_date",
+				DataType = typeof(DateTime),
+				AllowDBNull = false
+			});
+
+			datatable.Columns.Add(new DataColumn()
+			{
+				ColumnName = "opportunity_url",
+				DataType = typeof(string),
+				AllowDBNull = false
+			});
+
+			datatable.Columns.Add(new DataColumn()
+			{
+				ColumnName = "opportunity_description",
+				DataType = typeof(string),
+				AllowDBNull = false
+			});
+
+			datatable.Columns.Add(new DataColumn()
+			{
+				ColumnName = "opportunity_details",
+				DataType = typeof(string),
+				AllowDBNull = false
+			});
+
+			datatable.Columns.Add(new DataColumn()
+			{
+				ColumnName = "opportunity_company",
+				DataType = typeof(string),
+				AllowDBNull = false
+			});
+
+			datatable.Columns.Add(new DataColumn()
+			{
+				ColumnName = "opportunity_rate",
+				DataType = typeof(string),
+				AllowDBNull = false
+			});
+
+			return datatable;
+		}
+	}
+
+}
---
+++ OpportunityDataTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+
+	public class OpportunityDataTable
+	{
+		public DataTable createOpportunityDataTable()
+		{
+			var datatable = new DataTable();
+
+			datatable.column.Add(new DataColumn())
+	
+	}
+	}
+
+}
